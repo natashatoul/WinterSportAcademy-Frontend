0366b5d55af6ec69bb71830597bdca9aff3cea5b
natashatoul <toulnatalia@gmail.com>
2026-04-24 15:01:11 +0100

    chore(deploy): configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/WinterSportAcademy-Frontend/',
   plugins: [react()],
   test: {
     globals: true,
@@ -10,4 +11,3 @@ export default defineConfig({
     setupFiles: './src/setupTests.js',
   },
 })
-
